bc5e4bf43a56a114d7cbd80a5c484d3978e2c9e1
pamarthimanasalakshmi99-beep <pamarthimanasalakshmi99@gmail.com>
2026-04-30 23:50:26 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,18 @@
+# Day 1 - Basics (Variables, Data Types, Input/Output)
+
+# Taking input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Display output
+print("My name is", name)
+print("I am", age, "years old")
+
+# Data types example
+city = "Vijayawada"   # string
+marks = 95.5         # float
+is_student = True    # boolean
+
+print("City:", city)
+print("Marks:", marks)
+print("Student:", is_student)
